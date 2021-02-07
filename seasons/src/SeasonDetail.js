2b3f70b660cd91517e6d8b3e33df7a38238560d2
Aleksandar Andjelkovic <tobamaestro@hotmail.com>
2021-02-07 20:08:51 +0100

    State, error handling, class components

---
+++ seasons/src/SeasonDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SeasonDetail = () => {
+  return <div>Season Detail</div>;
+};
+
+export default SeasonDetail;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div>Hey there!</div>;
-};
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
+//   return <div>Hey there!</div>;
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //  This is the SINGLE ONLY TIME we assign a value directly to the state var - at init
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }), //  This is how we update state after init - always with setState
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  //  render() is mandatory to define in React
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
