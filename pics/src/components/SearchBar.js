cfcae1fc16a105f8668b57c33fa4ae0459a7f4fc
Aleksandar Andjelkovic <tobamaestro@hotmail.com>
2021-02-15 18:38:42 +0100

    Invoking callbacks in child components

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,19 +3,21 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onFormSubmit(event) {
+  onFormSubmit = (event) => {
     event.preventDefault();
-
-    console.log(this.state.term);
-  }
+    /*
+      1. We render the SearchBar with a prop named onSearch
+      2. When user submits search, we invoke this function (with <form onSubmit={this.onFormSubmit})
+      3. From within, we call the Parent function (with a prop name that is passed)
+      4. Handle on stuff in the Parent
+    */
+    this.props.onSearch(this.state.term); //  When referencing props from a class-based component, we PREFIX it with THIS
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form
-          onSubmit={(event) => this.onFormSubmit(event)}
-          className="ui form"
-        >
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
