f2d1b015155a7b9a8e56a60a80a102780fcac3b3
Aleksandar Andjelkovic <tobamaestro@hotmail.com>
2021-02-18 18:04:06 +0100

    Fixing grid row spans and img dimensions with React refs - finished pics app

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,11 +1,29 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    //    img not loaded yet, cannot access height so listener
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10); //  cause height is grid-auto-rows: 150px;
+    this.setState({ spans });
+  };
+
   render() {
     const { urls, description } = this.props.image;
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
