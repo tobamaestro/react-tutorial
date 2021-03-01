9b64d802e1222660439c4c4fa6c52967717c88dd
Aleksandar Andjelkovic <tobamaestro@hotmail.com>
2021-03-01 20:47:45 +0100

    Finishing touches, added iframe and layout for videos

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -7,16 +7,23 @@ import youtube from "../apis/youtube";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
-  onTermSubmit = async (term) => {
-    console.log(term);
+  defaultTerm = "chill jazz";
 
+  componentDidMount() {
+    this.onTermSubmit(this.defaultTerm);
+  }
+
+  onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
@@ -27,11 +34,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onSearch={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
