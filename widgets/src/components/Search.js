c1f566e747343ef66fbf5eb1a1b8da5b1d7efc22
Aleksandar Andjelkovic <tobamaestro@hotmail.com>
2021-03-05 16:40:04 +0100

    Render search results

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -20,8 +20,29 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    search();
+    if (term) {
+      search();
+    }
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -35,6 +56,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
